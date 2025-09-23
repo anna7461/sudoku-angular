f26087a8bed75a7ca6a1dd59e477ef0d11b6a9c0
anna7461 <a.ch7461@gmail.com>
2025-09-23 13:14:12 +0400

    feat: initialize Android project structure with Capacitor integration
    - Added Android project setup, including Gradle configuration and basic project files.
    - Integrated Capacitor for cross-platform support.
    - Included AndroidManifest, Gradle files, and resource assets.
    - Configured WebView and essential dependencies for basic functionality.
    - Added `.gitignore` for Android-specific files and directories.

---
+++ android/app/src/main/java/com/anna/sudoku/MainActivity.java
@@ -0,0 +1,5 @@
+package com.anna.sudoku;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anna.sudoku',
+  appName: 'Sudoku',
+  webDir: 'dist/sudoku-angular/browser'
+};
+
+export default config;
