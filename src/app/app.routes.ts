dd5267ef9777bf2abf191711a5697f25ec0a683a
anna7461 <a.ch7461@gmail.com>
2025-09-23 15:47:12 +0400

    feat: add privacy policy route to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './sudoku/components/dashboard/dashboard.component';
 import { SudokuComponent } from './sudoku/sudoku.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'sudoku', component: SudokuComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: '**', redirectTo: '' }
 ];
